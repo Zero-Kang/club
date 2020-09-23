785610159148ef1d15311416a1bc8df9fcb42d21
ckck23 <kangckck23@gmail.com>
2020-09-23 22:37:44 +0900

    Entity created

---
+++ src/main/java/org/zerock/club/entity/ClubMember.java
@@ -0,0 +1,40 @@
+package org.zerock.club.entity;
+
+import lombok.*;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class ClubMember extends BaseEntity {
+
+    @Id
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    private boolean fromSocial;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<ClubMemberRole> roleSet;
+
+    public void addMemberRole(ClubMemberRole role){
+
+        if(roleSet == null){
+            roleSet = new HashSet<>();
+        }
+        roleSet.add(role);
+
+    }
+}
